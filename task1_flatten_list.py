23380bd528a8d2e3a9a3c2c57ad68f4c31c99ddc
hszan <b@g>
2024-03-23 12:07:43 +0100

    korekta

--- task1_flatten_list.py
+++ task1_flatten_list.py
@@ -2,7 +2,7 @@ list1 = [1, [2, 3, [4, [5, 6], 7]], 8, 9, [[10]]]
 
 def flat_list(lst, re_list = []):
     for item in lst:
-        if type(item) is int:
+        if isinstance(item, (int, float, str)):
             re_list.append(item)
         else:
             flat_list(item)
